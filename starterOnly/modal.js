0273df1325cf38e9c4adf60e1f8ac0fd2cfcfda5
Johan <johan.peraldi@gmail.com>
2022-05-04 18:33:33 +0200

    Add modal close button functionality

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalButton = document.querySelector("span.close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,7 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// close modal form
+closeModalButton.addEventListener("click", () => {
+  modalbg.style.display = "none";
+});
