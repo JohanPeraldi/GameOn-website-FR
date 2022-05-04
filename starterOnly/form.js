c06af31f248d23230e51e33f27bc5bf3a7b3d5b6
Johan <johan.peraldi@gmail.com>
2022-05-05 00:27:44 +0200

    Add first & last names validation logic

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -1,4 +1,40 @@
 /**
+ * First name validation
+ */
+
+// Select input with id="first"
+const firstNameInput = document.getElementById('first');
+firstNameInput.addEventListener('blur', ($event) => {
+  if ($event.target.value.length > 1) {
+    console.log('First name is valid');
+    firstNameInput.style.border = '2px solid green';
+    firstNameInput.style.backgroundColor = '#E6FFEA';
+  } else {
+    console.log('First name is invalid');
+    firstNameInput.style.border = '2px solid red';
+    firstNameInput.style.backgroundColor = '#FFE6E6';
+  }
+});
+
+/**
+ * Last name validation
+ */
+
+// Select input with id="last"
+const lastNameInput = document.getElementById('last');
+lastNameInput.addEventListener('blur', ($event) => {
+  if ($event.target.value.length > 1) {
+    console.log('Last name is valid');
+    lastNameInput.style.border = '2px solid green';
+    lastNameInput.style.backgroundColor = '#E6FFEA';
+  } else {
+    console.log('Last name is invalid');
+    lastNameInput.style.border = '2px solid red';
+    lastNameInput.style.backgroundColor = '#FFE6E6';
+  }
+});
+
+/**
  * Email validation
  */
 
