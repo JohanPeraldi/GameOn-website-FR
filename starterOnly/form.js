a7e85c0326c71f9040dd39226dd4c9435ae64ecd
Johan <johan.peraldi@gmail.com>
2022-05-05 18:18:16 +0200

    Add JS first & last names validation with message display

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -1,58 +1,147 @@
 /**
+ * DOM Elements
+ */
+
+// "Prénom" input
+const firstNameInput = document.getElementById('first');
+
+// Parent of "prénom" input
+const firstNameInputParent = firstNameInput.parentElement;
+
+// Create a span element that will contain a message
+// to be inserted after input element
+const firstNameInputMessage = document.createElement('span');
+// Give it an id
+firstNameInputMessage.setAttribute('id', 'first-name-input-message');
+// The message to display inside the created 'span' element
+const nameMessageEmpty = 'Ce champ ne peut pas être vide';
+const nameMessageShort = 'Veuillez entrer au moins deux caractères';
+// Style the message
+firstNameInputMessage.style.fontSize = '14px';
+firstNameInputMessage.style.color = 'red';
+
+// "Nom" input
+const lastNameInput = document.getElementById('last');
+
+// Parent of "nom" input
+const lastNameInputParent = lastNameInput.parentElement;
+
+// Create a span element that will contain a message
+// to be inserted after input element
+const lastNameInputMessage = document.createElement('span');
+// Give it an id
+lastNameInputMessage.setAttribute('id', 'last-name-input-message');
+// Style the message
+lastNameInputMessage.style.fontSize = '14px';
+lastNameInputMessage.style.color = 'red';
+
+// "Email" input
+const emailInput = document.getElementById('email');
+
+
+/**
  * First name validation
  */
 
-// Select input with id="first"
-const firstNameInput = document.getElementById('first');
+// A variable to store the validity of the input
+let firstNameIsValid = false;
+
+// Check that, when clicking outside "first name" input box,
+// entered first name has at least two characters
 firstNameInput.addEventListener('blur', ($event) => {
   if ($event.target.value.length > 1) {
-    console.log('First name is valid');
-    firstNameInput.style.border = '2px solid green';
+    console.log('Le champ du prénom est valide');
+    firstNameIsValid = true;
+    firstNameInput.style.border = '1px solid green';
     firstNameInput.style.backgroundColor = '#E6FFEA';
   } else {
-    console.log('First name is invalid');
-    firstNameInput.style.border = '2px solid red';
+    firstNameInput.style.border = '1px solid red';
     firstNameInput.style.backgroundColor = '#FFE6E6';
+    if ($event.target.value.length === 1) {
+      console.log('Veuillez entrer au moins deux caractères');
+      firstNameInputMessage.textContent = nameMessageShort;
+      firstNameInputParent.appendChild(firstNameInputMessage);
+    } else {
+      console.log('Le champ prénom ne peut pas être vide');
+      firstNameInputMessage.textContent = nameMessageEmpty;
+      firstNameInputParent.appendChild(firstNameInputMessage);
+    }
   }
 });
+
+// 'Focus' event listener aimed at removing any existing 'firstNameInputMessage'
+// element created when entering an invalid first name
+firstNameInput.addEventListener('focus', () => {
+  // Check whether a 'span' element exists at the very end of the parent element
+  // console.log(firstNameInputParent.lastElementChild);
+  console.log(document.getElementById('first-name-input-message')); // returns null if does not exist
+  if (document.getElementById('first-name-input-message') !== null) {
+    // Remove element
+    firstNameInputParent.removeChild(firstNameInputMessage);
+  }
+});
+
 
 /**
  * Last name validation
  */
 
-// Select input with id="last"
-const lastNameInput = document.getElementById('last');
+// A variable to store the validity of the input
+let lastNameIsValid = false;
+
+// Check that, when clicking outside "last name" input box,
+// entered last name has at least two characters
 lastNameInput.addEventListener('blur', ($event) => {
   if ($event.target.value.length > 1) {
-    console.log('Last name is valid');
-    lastNameInput.style.border = '2px solid green';
+    console.log('Le champ du nom est valide');
+    lastNameIsValid = true;
+    lastNameInput.style.border = '1px solid green';
     lastNameInput.style.backgroundColor = '#E6FFEA';
   } else {
-    console.log('Last name is invalid');
-    lastNameInput.style.border = '2px solid red';
+    lastNameInput.style.border = '1px solid red';
     lastNameInput.style.backgroundColor = '#FFE6E6';
+    if ($event.target.value.length === 1) {
+      console.log('Veuillez entrer au moins deux caractères');
+      lastNameInputMessage.textContent = nameMessageShort;
+      lastNameInputParent.appendChild(lastNameInputMessage);
+    } else {
+      console.log('Le champ nom ne peut pas être vide');
+      lastNameInputMessage.textContent = nameMessageEmpty;
+      lastNameInputParent.appendChild(lastNameInputMessage);
+    }
   }
 });
+
+// 'Focus' event listener aimed at removing any existing 'lastNameInputMessage'
+// element created when entering an invalid last name
+lastNameInput.addEventListener('focus', () => {
+  // Check whether a 'span' element exists at the very end of the parent element
+  if (document.getElementById('last-name-input-message') !== null) {
+    // Remove element
+    lastNameInputParent.removeChild(lastNameInputMessage);
+  }
+});
+
 
 /**
  * Email validation
  */
 
-// Validate email with x@x.x pattern
+// Check that, when clicking outside "email" input box,
+// entered email address follows x@x.x pattern
+// (white spaces not allowed, second @ character not allowed)
 const emailIsValid = (email) => {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
 
-// Select input with id="email"
-const emailInput = document.getElementById('email');
 emailInput.addEventListener('blur', ($event) => {
   if (emailIsValid($event.target.value)) {
-    console.log('Email is valid');
-    emailInput.style.border = '2px solid green';
+    console.log('Le champ email est valide');
+    emailInput.style.border = '1px solid green';
     emailInput.style.backgroundColor = '#E6FFEA';
   } else {
-    console.log('Email is invalid');
-    emailInput.style.border = '2px solid red';
+    console.log('Veuillez entrer une adresse email valide du type ab@cd.ef');
+    emailInput.style.border = '1px solid red';
     emailInput.style.backgroundColor = '#FFE6E6';
   }
 });
